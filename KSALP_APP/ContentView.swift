a226f013ed23fe6969dd1536c40fbc95922f5232
NiklasZaugg <Niklas_zaugg@sluz.ch>
2024-02-24 13:55:44 +0100

    Initial Commit

---
+++ KSALP_APP/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KSALP_APP
+//
+//  Created by Niklas on 24.02.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, worldddd!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KSALP_APP/KSALP_APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  KSALP_APPApp.swift
+//  KSALP_APP
+//
+//  Created by Niklas on 24.02.24.
+//
+
+import SwiftUI
+
+@main
+struct KSALP_APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
