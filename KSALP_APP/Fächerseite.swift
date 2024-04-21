419fcf1af949cc7ccb7692bcf9480351bfc0966d
NiklasZaugg <Niklas_zaugg@sluz.ch>
2024-04-21 23:00:04 +0200

    Durchschnitssberechnungsproblem wurde durch ".onDisappear(perform: calculateAverage))" gelöst. Kleine Änderungen, wie anstatt kein Prüfungsname immer "Neue Note" mithilfe diesem Befehl: self.newName = "Neue Note" // Setzt den Namen jedes Mal zurück, bevor das Sheet geöffnet wird. UI der gradedetailview wurde angepasst mit Gewichtung, Datum und Bewertung. Das UI der Liste der Prüfungen wurde angepasst.

--- KSALP_APP/Fächerseite.swift
+++ KSALP_APP/Fächerseite.swift
@@ -15,137 +15,148 @@ struct ContentView: View {
     @State private var showActionSheet: Bool = false // Steuert die Anzeige des ActionSheets (Titel)
     @State private var showingAddGradeSheet: Bool = false // Steuert die Anzeige des Sheets für das Hinzufügen neuer Noten (Prüfung)
     @State private var showingEditSubjectSheet: Bool = false // Steuert die Anzeige des Sheets für das Bearbeiten des Fachnamens
-    @State private var newName: String = "" // Eingabefeld für den Namen der neuen Note
+    @State private var newName: String = "Neue Note" // Eingabefeld für den Namen der neuen Note
     @State private var newScore: String = "" // Eingabefeld für die Punktzahl der neuen Note
     @State private var newWeight: String = "1.0" // Eingabefeld für die Gewichtung der neuen Note
     @State private var newDate: Date = Date() // Standart - heutiges Datum
 
 
     var body: some View {
-        VStack {
-            Button(action: {
-                self.showActionSheet = true // Zeigt das ActionSheet Titel an
-            }) {
-                HStack {
-                    Text(subjectName).font(.title)
-                    Image(systemName: "chevron.down")
-                }.foregroundColor(.black)
-            }
-            .padding()
-            .actionSheet(isPresented: $showActionSheet) {
-                ActionSheet(title: Text("Aktion wählen"), message: nil, buttons: [
-                    .default(Text("Fachtitel bearbeiten")) {
-                        self.editingSubjectName = self.subjectName
-                        self.showingEditSubjectSheet = true
-                    },
-                    .destructive(Text("Fach löschen")) {
-                        self.grades.removeAll() // Löscht alle Noten
-                        calculateAverage() // Berechnet den Durchschnitt neu
-                    },
-                    .cancel()
-                ])
-            }
-            .sheet(isPresented: $showingEditSubjectSheet) { //Aktiviert das Sheet Fachtitel bearbeiten
-                NavigationView {
+        NavigationStack{
+            VStack {
+                Button(action: {
+                    self.showActionSheet = true // Zeigt das ActionSheet Titel an
+                }) {
+                    HStack {
+                        Text(subjectName).font(.title)
+                        Image(systemName: "chevron.down")
+                    }.foregroundColor(.black)
+                }
+                .padding()
+                .actionSheet(isPresented: $showActionSheet) {
+                    ActionSheet(title: Text("Aktion wählen"), message: nil, buttons: [
+                        .default(Text("Fachtitel bearbeiten")) {
+                            self.editingSubjectName = self.subjectName
+                            self.showingEditSubjectSheet = true
+                        },
+                        .destructive(Text("Fach löschen")) {
+                            self.grades.removeAll() // Löscht alle Noten
+                            calculateAverage() // Berechnet den Durchschnitt neu
+                        },
+                        .cancel()
+                    ])
+                }
+                .sheet(isPresented: $showingEditSubjectSheet) { //Aktiviert das Sheet Fachtitel bearbeiten
+                    NavigationView {
+                        VStack {
+                            TextField("Fachtitel eingeben", text: $editingSubjectName)
+                                .multilineTextAlignment(.center)
+                                .padding()
+                            
+                        }
+                        .navigationBarTitle("Fachtitel bearbeiten", displayMode: .inline)
+                        .navigationBarItems(
+                            leading: Button("Abbrechen") {
+                                showingEditSubjectSheet = false // Schliesst das Sheet, ohne Änderungen zu speichern
+                            },
+                            trailing: Button("Fertig") {
+                                subjectName = editingSubjectName
+                                showingEditSubjectSheet = false // Speichert die Änderungen und schliesst das Sheet
+                            }
+                        )
+                    }
+                }
+                
+                Spacer()
+                
+                // Durchschnittsanzeige
+                Text("Durchschnitt: \(String(format: "%.2f", average))") // Zeigt den Durchschnittswert
+                
+                // Liste der Noten
+                ScrollView {
                     VStack {
-                        TextField("Fachtitel eingeben", text: $editingSubjectName)
-                            .multilineTextAlignment(.center)
-                            .padding()
-                        
-                    }
-                    .navigationBarTitle("Fachtitel bearbeiten", displayMode: .inline)
-                    .navigationBarItems(
-                        leading: Button("Abbrechen") {
-                            showingEditSubjectSheet = false // Schliesst das Sheet, ohne Änderungen zu speichern
-                        },
-                        trailing: Button("Fertig") {
-                            subjectName = editingSubjectName
-                            showingEditSubjectSheet = false // Speichert die Änderungen und schliesst das Sheet
+                        ForEach(grades.indices, id: \.self) { index in
+                            NavigationLink(destination: GradeDetailView(grade: $grades[index])
+                                .onDisappear(perform: calculateAverage)) { // Berechnet den Durchschnitt neu, wenn Details geändert werden
+                                HStack {
+                                    VStack(alignment: .leading) {
+                                        Text(grades[index].name) // Anzeige des Namens
+                                            .frame(width: 100, alignment: .leading)
+                                        HStack {
+                                            Image(systemName: "scalemass") // Symbol für Gewichtung
+                                                .resizable()
+                                                .scaledToFit()
+                                                .frame(width: 15, height: 15)
+                                                .foregroundColor(.gray)
+                                            Text(String(format: "%.1f", grades[index].weight)) // Anzeige der Gewichtung
+                                                .font(.caption)
+                                                .foregroundColor(.gray)
+                                        }
+                                    }
+                                    Spacer() // Trennt den Namen und die Note optisch
+                                    Text(String(format: "%.2f", grades[index].score)) // Anzeige der Bewertung
+                                        .frame(alignment: .trailing)
+                                }
+                                .padding()
+                            } // Ende des NavigationLink
                         }
-                    )
+                    }
+                    .padding()
                 }
-            }
+                .foregroundColor(.black)
 
-            Spacer()
-            
-            // Durchschnittsanzeige
-            Text("Durchschnitt: \(String(format: "%.2f", average))") // Zeigt den Durchschnittswert
-            
-            // Liste der Noten
-            ScrollView {
-                VStack {
-                    ForEach(grades.indices, id: \.self) { index in
-                        HStack {
-                            Text(grades[index].name)
-                                .frame(width: 100, alignment: .leading)
-                            Spacer()
-                            Text(String(format: "%.2f", grades[index].score))
-                            Spacer()
-                            Text(String(format: "%.1f", grades[index].weight))
-                            Spacer()
-                            Text(grades[index].date, style: .date) // Anzeige des Datums
-                            Spacer()
-                            Button(action: {
-                                grades.remove(at: index) // Löschfunktion für die Note
-                                calculateAverage() // Berechnet den Durchschnitt neu
-                            }) {
-                                Image(systemName: "trash")
-                                    .foregroundColor(.red)
-                            }
-                        }
-                        .padding()
-                    }
+
+                
+                // Hinzufügen neuer Noten
+                Button(action: {
+                    self.newName = "Neue Note" // Setzt den Namen jedes Mal zurück, bevor das Sheet geöffnet wird
+                    self.showingAddGradeSheet = true // Aktiviert das Sheet zum Hinzufügen neuer Noten
+                }) {
+                    Text("Neue Note hinzufügen")
                 }
-            }
-            .padding()
-            .foregroundColor(.black)
-            
-            // Hinzufügen neuer Noten
-            Button(action: {
-                self.showingAddGradeSheet = true // Aktiviert das Sheet zum Hinzufügen neuer Noten
-            }) {
-                Text("Neue Note hinzufügen")
-            }
-            .padding()
-            .sheet(isPresented: $showingAddGradeSheet) { //Anzegige Sheet neue Noten
-                NavigationView {
-                    Form {
-                        Section(header: Text("Prüfungsdetails").font(.headline)) { //Formular standartform IOS
-                            TextField("Prüfung", text: $newName)
-                            TextField("Note", text: $newScore)
-                                .keyboardType(.decimalPad)
-                            HStack {
-                                Text("Gewichtung:")
-                                    .frame(width: 100, alignment: .leading) // Gibt dem Text eine feste Breite
-                                TextField("1.0", text: $newWeight)
+                .padding()
+                .sheet(isPresented: $showingAddGradeSheet) { //Anzegige Sheet neue Noten
+                    NavigationView {
+                        Form {
+                            Section(header: Text("Prüfungsdetails").font(.headline)) { //Formular standartform IOS
+                                TextField("Prüfung", text: $newName)
+                                TextField("Note", text: $newScore)
                                     .keyboardType(.decimalPad)
-                                    .multilineTextAlignment(.trailing)
-                                    .frame(width: 200) // Gibt dem TextField eine feste Breite für Konsistenz
+                                HStack {
+                                    Text("Gewichtung:")
+                                        .frame(width: 100, alignment: .leading) // Gibt dem Text eine feste Breite
+                                    TextField("1.0", text: $newWeight)
+                                        .keyboardType(.decimalPad)
+                                        .multilineTextAlignment(.trailing)
+                                        .frame(width: 200) // Gibt dem TextField eine feste Breite für Konsistenz
+                                }
+                                .padding(.vertical, 8) // Gleiches Padding für alle Elemente
+                                
+                                DatePicker("Datum der Prüfung", selection: $newDate, displayedComponents: .date)
                             }
-                            .padding(.vertical, 8) // Gleiches Padding für alle Elemente
-
-                            DatePicker("Datum der Prüfung", selection: $newDate, displayedComponents: .date)
-                        }
-                    }                    .navigationBarTitle("Neue Note hinzufügen", displayMode: .inline)
-                    .navigationBarItems(
-                        leading: Button("Abbrechen") {
-                            showingAddGradeSheet = false
-                        },
-                        trailing: Button("Fertig") {
-                            addGrade(name: newName, score: Double(newScore) ?? 0.0, weight: Double(newWeight) ?? 1.0, date: newDate)
-                            showingAddGradeSheet = false
-                            newName = ""
-                            newScore = ""
-                            newWeight = ""
-                        }
-                    )
+                        }                    .navigationBarTitle("Neue Note hinzufügen", displayMode: .inline)
+                            .navigationBarItems(
+                                leading: Button("Abbrechen") {
+                                    showingAddGradeSheet = false
+                                },
+                                trailing: Button("Fertig") {
+                                    addGrade(name: newName, score: Double(newScore) ?? 0.0, weight: Double(newWeight) ?? 1.0, date: newDate)
+                                    showingAddGradeSheet = false
+                                    newName = ""
+                                    newScore = ""
+                                    newWeight = ""
+                                }
+                            )
+                    }
                 }
+                
+                Spacer()
             }
-
-            Spacer()
         }
         .padding()
         .onAppear(perform: calculateAverage) // Berechnet den Durchschnitt beim ersten Laden der Ansicht
+        
+        
         .preferredColorScheme(.light) // Erzwingt Light Mode für View
     }
     
--- KSALP_APP/GradeDetailView.swift
+++ KSALP_APP/GradeDetailView.swift
@@ -5,4 +5,64 @@
 //  Created by Niklas on 20.04.24.
 //
 
-import Foundation
+import SwiftUI
+
+struct GradeDetailView: View {
+    @Binding var grade: Grade
+    @Environment(\.presentationMode) var presentationMode
+    
+    // Temporäre lokale Kopie für die Bearbeitung in der Detailansicht
+    @State private var tempGrade: Grade
+
+    // Initialisierung der lokalen Kopie im Konstruktor
+    init(grade: Binding<Grade>) {
+        self._grade = grade
+        self._tempGrade = State(initialValue: grade.wrappedValue)
+    }
+
+    var body: some View {
+            Form {
+                Section(header: Text("Details").font(.headline)) {
+                    HStack {
+                        Text("Prüfung:")
+                            .frame(width: 100, alignment: .leading)
+                        TextField("Prüfung", text: $tempGrade.name)
+                    }
+                    
+                    HStack {
+                        Text("Note:")
+                            .frame(width: 100, alignment: .leading)
+                        TextField("Note", value: $tempGrade.score, formatter: NumberFormatter.decimalFormatter)
+                    }
+                    
+                    HStack {
+                        Text("Gewichtung:")
+                            .frame(width: 100, alignment: .leading)
+                        TextField("Gewichtung", value: $tempGrade.weight, formatter: NumberFormatter.decimalFormatter)
+                    }
+                    
+                    HStack {
+                        Text("Datum:")
+                            .frame(width: 140, alignment: .leading)
+                        DatePicker("", selection: $tempGrade.date, displayedComponents: .date)
+                            .labelsHidden()  // Verbirgt das Label des DatePicker um eine sauberere UI zu gewährleisten
+                    }
+                }
+            }        .navigationBarTitle("Prüfungsdetails", displayMode: .inline)
+        .navigationBarItems(
+            trailing: Button("Fertig") {
+                self.grade = self.tempGrade  // Übertragen der Änderungen auf das Binding-Objekt
+                self.presentationMode.wrappedValue.dismiss()
+            }
+        )
+    }
+}
+
+extension NumberFormatter {
+    static let decimalFormatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        return formatter
+    }()
+}
+
